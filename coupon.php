4e6ea38e5ea2459a9aaf5bd91b31b933dd06a751
Ivan Baena <ivan.baena04@gmail.com>
2017-01-18 15:25:46 -0500

    updated coupon

--- coupon.php
+++ coupon.php
@@ -28,6 +28,9 @@ $response = file_get_contents('http://coupons2.smartsource.com/smartsource2/Toke
             <section class="intro-content col-xs-12">
               THANK YOU FOR TAKING THE TIME TO TAKE OUR SURVEY FOR FERRERO ROCHER<sup id="bold-reg">&reg;</sup> VALENTINE'S DAY HOLLOW HEARTS!
               <p class="small-intro">YOUR OPINION IS VERY IMPORTANT TO US.</p>
+              <p>
+                To show our appreciation, please click on the button below to get your coupon for a FREE 12 count Ferrero Rocher® (5.3 OZ) or Ferrero Collection (4.6 OZ).
+              </p>
             </section>
             <section class="second-content col-xs-12">
               <div class="col-md-12 col-xs-12">
